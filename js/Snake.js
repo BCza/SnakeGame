330a8d15392cfe2de83887b7a63ee07fb66be29d
Brian Czaicki <bczaicki@outlook.com>
2020-11-04 04:14:32 -0600

    Changing snake to green

--- js/Snake.js
+++ js/Snake.js
@@ -1,21 +1,19 @@
 // Snake Class
-var Snake = (function() 
-{
-    /************************************************  Constructors ************************************/
-    function Snake() 
-    {
-        this._color = "Black";
-    };
+var Snake = (function () {
+  /************************************************  Constructors ************************************/
+  function Snake() {
+    this._color = "green";
+  }
 
-    function Snake(color,direction) 
-    {
-        this._color = color;
-        this._direction = direction;
-        this._length = 4;
-        this._body = [];
-        this._head = 0;
-        this._tail = this._length; 
-    };
+  function Snake(color, direction) {
+    this._color = color;
+    this._direction = direction;
+    this._length = 4;
+    this._body = [];
+    this._head = 0;
+    this._tail = this._length;
+  }
 
-    return Snake;
+  return Snake;
 })();
+
